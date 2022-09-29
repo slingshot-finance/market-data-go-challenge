d39bd6e116d82c62c61cf82a2409ba52f60fb3aa
Mother <tarasworonjanski@gmail.com>
2022-09-30 07:26:10 +1000

    added description

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # market-data-go-challenge
 Simple go challenge to complete.
+
+Fill in the blanks and get "TestApp" Unit Test to pass. 
+
+A quick guide: 
+    * Input Handler listens to a stream of inputs from a go-channel and pushes Task functions into a Task Queue to handle the inputs. 
+    * Task Manager manages & executes any Tasks sent to the Task Queue
+    * Results from Tasks are pushed to a Map (called Storage).
+    * Task Queue has a size limit, don't push tasks into the Queue if its full.
+    * Task Manager is also able to drop values from Storage based on a given filter function.
+    * Storage has a size cap, remove the oldest known value if cap is reached.
+
+
+Challenge is expected to be finished within 1 hour.
--- input/input_handler.go
+++ input/input_handler.go
@@ -1,35 +1,17 @@
 package input
 
-import (
-	"context"
-	"fmt"
-
-	task "challenge/task"
-
-	"github.com/ethereum/go-ethereum/common/hexutil"
-)
-
-/*
-	provide access to the necessary funtions provided by the TaskManager
-*/
-type TaskManager interface {
-	PushToQueue(task.TaskTemplate)
-}
-
 type InputHandler struct {
-	tskManager TaskManager
-	InputChan  chan []byte
-	ErrorChan  chan error
+	InputChan chan []byte
+	ErrorChan chan error
 }
 
 /*
 	initialises InputHandler
 */
-func InitInputHandler(taskManager TaskManager) *InputHandler {
+func InitInputHandler() *InputHandler {
 	return &InputHandler{
-		tskManager: taskManager,
-		InputChan:  make(chan []byte),
-		ErrorChan:  make(chan error),
+		InputChan: make(chan []byte),
+		ErrorChan: make(chan error),
 	}
 }
 
@@ -47,18 +29,4 @@ func (h *InputHandler) ParseInput() {
 			- Ox0b --> push taskB to queue
 	*/
 
-	for {
-		input, ok := <-h.InputChan
-		if !ok {
-			h.ErrorChan <- fmt.Errorf("inputChan has been closed")
-			return
-		}
-
-		switch hexutil.Encode(input[:1]) {
-		case "0x0a":
-			go h.tskManager.PushToQueue(func(ctx context.Context) (string, []byte) { return task.TaskA(ctx, input) })
-		case "0x0b":
-			go h.tskManager.PushToQueue(func(ctx context.Context) (string, []byte) { return task.TaskB(ctx, input) })
-		}
-	}
 }
